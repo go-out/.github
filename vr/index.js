89f03df358f0cf5381ddbb48f4cabb06386543cd
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-12-23 18:29:51 +0900

    relax

--- vr/index.js
+++ vr/index.js
@@ -10,37 +10,18 @@ async function indexJSON(requestURL) {
 }
 
 function videoAll(obj) {
-    const randomdRaggable = document.querySelector('main fieldset');
+    const main = document.querySelector('main');
 
     const playAll = obj.play;
     for (let i = 0; i < playAll.length; i++) {
-        const input = document.createElement('input');
-        input.setAttribute('type', 'radio');
-        input.setAttribute('name', 'video');
-        input.id = playAll[i].id;
-        input.value = playAll[i].id;
-        randomdRaggable.appendChild(input);
-
-        const label = document.createElement('label');
-        label.setAttribute('for', playAll[i].id);
-        randomdRaggable.appendChild(label);
-
         const video = document.createElement('video');
         video.setAttribute('playsinline', 'true');
-        label.appendChild(video);
+        main.appendChild(video);
 
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             video.muted = true;
             video.setAttribute('muted', 'true');
         }
-
-        const canvas = document.querySelector("main canvas");
-        let canvasCtx = canvas.getContext('2d');
-
-        function canvasUpdate() {
-            canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
-            requestAnimationFrame(canvasUpdate);
-        };
 
         if (playAll[i].src) {
             let ii = 0;
@@ -60,19 +41,10 @@ function videoAll(obj) {
                     ii++;
                     source.src = obj.directory + playAll[i].src[ii];
                 }
-                canvasUpdate();
                 video.load();
                 video.play();
             }, false);
         }
-
-        if (i === 0) {
-            canvasUpdate();
-        }
-
-        video.addEventListener('click', function () {
-            canvasUpdate();
-        })
     }
 }
 
@@ -115,37 +87,4 @@ function playThis(obj) {
             iii.pause();
         })
     }
-
-    window.onresize = tmResize;
-    function tmResize() {
-        if (typeof pageResize == "function") {
-            pageResize();
-        }
-    }
-
-    function windowScreen() {
-        const canvas = document.querySelector("main canvas");
-        canvas.width = window.innerHeight / obj.width;
-        canvas.height = window.innerHeight / obj.height;
-    }
-
-    function pageResize() {
-        windowScreen();
-    }
-    windowScreen();
 }
-
-window.addEventListener("load", () => {
-    const scrollElement = document.querySelector('main fieldset');
-    scrollElement.addEventListener('wheel', (e) => {
-        if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
-        const maxScrollLeft = scrollElement.scrollWidth - scrollElement.clientWidth;
-        if (
-            (scrollElement.scrollLeft <= 0 && e.deltaY < 0) ||
-            (scrollElement.scrollLeft >= maxScrollLeft && e.deltaY > 0)
-        )
-            return;
-        e.preventDefault();
-        scrollElement.scrollLeft += e.deltaY;
-    });
-});
