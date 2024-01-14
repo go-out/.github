c4f41ddd784b2fc524aa03ee11edb119668c2eef
verygois <sorryforthedelayinsending.vg.pe.hu>
2024-01-14 12:43:42 +0900

    map

--- osaka/alps/index.js
+++ osaka/alps/index.js
@@ -7,21 +7,6 @@ const osakaFerry = {
             'type': 'Feature',
             'geometry': {
                 'type': 'Point',
-                'coordinates': [135.4327054373283, 34.65756057444443]
-            },
-            'properties': {
-                'title': 'HELLO CYCLING',
-                'address': '天保山公園',
-                'date': '<a href="https://maps.app.goo.gl/WqFtKxV9TmNxHGyP8" target="_blank">Google Map</a>',
-                'iconSize': ['profile/img/hello.jpg', '2.5rem', '2.5rem'],
-                'tags': 'hello',
-                'zoom': 16,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
                 'coordinates': [135.43561508622975, 34.65327525828587]
             },
             'properties': {
@@ -37,12 +22,12 @@ const osakaFerry = {
             'type': 'Feature',
             'geometry': {
                 'type': 'Point',
-                'coordinates': [135.43109856854932, 34.658746822143925]
+                'coordinates': [135.43264396767563, 34.658031102311625]
             },
             'properties': {
-                'title': '天保山渡船場',
-                'address': '安治川（港区築港3丁目 ⇄ 此花区桜島3丁目）を 15 ~ 30分毎 運航',
-                'date': '<a href="osaka/ferry/?name=tempozan">more info</a>',
+                'title': '天保山',
+                'address': '天保山公園にある人工的に土を積み上げて造られた山（築山）。標高4.53m（15尺）',
+                'date': '<a href="?id=park&area=osaka&name=tempozan">more info</a>',
                 'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
                 'tags': 'vr',
                 'zoom': 16,
@@ -52,14 +37,14 @@ const osakaFerry = {
             'type': 'Feature',
             'geometry': {
                 'type': 'Point',
-                'coordinates': [135.46042671655556,34.66982106364067]
+                'coordinates': [135.50286230415117, 34.63547074276174]
             },
             'properties': {
-                'title': 'HELLO CYCLING',
-                'address': '大阪ベイタワー',
-                'date': '<a href="https://maps.app.goo.gl/dFgQiP7XdzFyKsLs6" target="_blank">Google Map</a>',
-                'iconSize': ['profile/img/hello.jpg', '2.5rem', '2.5rem'],
-                'tags': 'hello',
+                'title': '聖天山',
+                'address': '標高14mの低山。頂上には、真言宗単立の寺院「正圓寺（しょうえんじ）」があります。',
+                'date': '<a href="?id=legacy&area=osaka&name=shoutenyama">more info</a>',
+                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
+                'tags': 'vr',
                 'zoom': 16,
             }
         },
@@ -67,14 +52,14 @@ const osakaFerry = {
             'type': 'Feature',
             'geometry': {
                 'type': 'Point',
-                'coordinates': [135.4612218445896,34.67107463855454]
+                'coordinates': [135.5359834816478, 34.654466425764554]
             },
             'properties': {
-                'title': 'HELLO CYCLING',
-                'address': 'べんてんひろば駐輪場',
-                'date': '<a href="https://maps.app.goo.gl/FyM356NmJnVWw1q36" target="_blank">Google Map</a>',
-                'iconSize': ['profile/img/hello.jpg', '2.5rem', '2.5rem'],
-                'tags': 'hello',
+                'title': '御勝山',
+                'address': '御勝山古墳、後円部の墳丘。標高14m。古墳の中央を勝山通が貫き、前方部は崩されて「御勝山南公園」になっています。',
+                'date': '<a href="?id=legacy&area=osaka&name=okachiyama">more info</a>',
+                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
+                'tags': 'vr',
                 'zoom': 16,
             }
         },
@@ -82,14 +67,14 @@ const osakaFerry = {
             'type': 'Feature',
             'geometry': {
                 'type': 'Point',
-                'coordinates': [135.4793856742926, 34.66324891101114]
+                'coordinates': [135.49682232943638, 34.621565133427865]
             },
             'properties': {
-                'title': 'HELLO CYCLING',
-                'address': 'セブンイレブン　大阪三軒家東4丁目店',
-                'date': '<a href="https://maps.app.goo.gl/wJ2tP2Usp9vfYHSi9" target="_blank">Google Map</a>',
-                'iconSize': ['profile/img/hello.jpg', '2.5rem', '2.5rem'],
-                'tags': 'hello',
+                'title': '帝塚山',
+                'address': '帝塚山古墳の墳丘。標高19.88m。 ※古墳内への立ち入りには、「財団法人住吉村常盤会」への申請が必要です。',
+                'date': '<a href="?id=legacy&area=osaka&name=tezukayama">more info</a>',
+                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
+                'tags': 'vr',
                 'zoom': 16,
             }
         },
@@ -97,141 +82,14 @@ const osakaFerry = {
             'type': 'Feature',
             'geometry': {
                 'type': 'Point',
-                'coordinates': [135.46246523088013, 34.656554003191715]
+                'coordinates': [135.51234003447854, 34.651695429506546]
             },
             'properties': {
-                'title': '甚兵衛渡船場',
-                'address': '尻無川（大正区泉尾7丁目 ⇄ 港区福崎1丁目）を 随時 ~ 15分毎 運航',
-                'date': '<a href="osaka/ferry/?name=zonbee">more info</a>',
+                'title': '茶臼山',
+                'address': '天王寺公園にある茶臼山古墳の墳丘。標高26m。古墳も公園の一部として遊歩道が整備されています。',
+                'date': '<a href="?id=legacy&area=osaka&name=tezukayama">more info</a>',
                 'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
                 'tags': 'vr',
-                'id': 'erky9l9h0KQ',
-                'zoom': 16.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.45854569618052, 34.646324348745]
-            },
-            'properties': {
-                'title': '千歳渡船場',
-                'address': '大正内港（大正区鶴町4丁目 ⇄ 同区北恩加島2丁目）を 10 ~ 20分毎 運航',
-                'date': '<a href="osaka/ferry/?name=chitose">more info</a>',
-                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
-                'tags': 'vr',
-                'id': 'pHLZszPeUEQ',
-                'zoom': 16,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.4758149813568, 34.650945663281405]
-            },
-            'properties': {
-                'title': 'HELLO CYCLING',
-                'address': '千島ガーデンモール',
-                'date': '<a href="https://maps.app.goo.gl/HYWBuQFTiZQwQKdb8" target="_blank">Google Map</a>',
-                'iconSize': ['profile/img/hello.jpg', '2.5rem', '2.5rem'],
-                'tags': 'hello',
-                'zoom': 16,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.47882918904605, 34.64972048487182]
-            },
-            'properties': {
-                'title': '落合上渡船場',
-                'address': '木津川（大正区千島1丁目 ⇄ 西成区北津守4丁目）を 10 ~ 15分毎 運航',
-                'date': '<a href="osaka/ferry/?name=ochiaikami">more info</a>',
-                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
-                'tags': 'vr',
-                'id': 'tkVySMeh9Js',
-                'zoom': 16.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.47911523706694, 34.643336261453044]
-            },
-            'properties': {
-                'title': '落合下渡船場',
-                'address': '木津川（大正区平尾1丁目 ⇄ 西成区津守2丁目）を 10 ~ 15分毎 運航',
-                'date': '<a href="osaka/ferry/?name=ochiaishimo">more info</a>',
-                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
-                'tags': 'vr',
-                'id': 'OmS0ifSabJo',
-                'zoom': 16.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.47722843823783, 34.62360491707443]
-            },
-            'properties': {
-                'title': 'HELLO CYCLING',
-                'address': 'サイクルベースあさひ北加賀屋店',
-                'date': '<a href="https://maps.app.goo.gl/H4ngonKhXimYdKEM6" target="_blank">Google Map</a>',
-                'iconSize': ['profile/img/hello.jpg', '2.5rem', '2.5rem'],
-                'tags': 'hello',
-                'zoom': 16,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.47569470365949, 34.631793416745694]
-            },
-            'properties': {
-                'title': '千本松渡船場',
-                'address': '木津川（大正区南恩加島1丁目 ⇄ 西成区南津守2丁目）を 10 ~ 15分毎 運航',
-                'date': '<a href="osaka/ferry/?name=senbonmatsu">more info</a>',
-                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
-                'tags': 'vr',
-                'id': 'SNw7eE2K0c0',
-                'zoom': 16,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.45798171771384, 34.632950453586275]
-            },
-            'properties': {
-                'title': '船町渡船場',
-                'address': '木津川運河（大正区鶴町1丁目 ⇄ 同区船町1丁目）を 10 ~ 20分毎 運航',
-                'date': '<a href="osaka/ferry/?name=funamachi">more info</a>',
-                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
-                'tags': 'vr',
-                'id': 'Z8Von0j_br8',
-                'zoom': 17,
-            }
-        },
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.46177705603935, 34.62555875027091]
-            },
-            'properties': {
-                'title': '木津川渡船場',
-                'address': '木津川（大正区船町1丁目 ⇄ 住之江区平林北1丁目）を 10 ~ 45分毎 運航',
-                'date': '<a href="osaka/ferry/?name=kizugawa">more info</a>',
-                'iconSize': ['osaka/ferry/icon.png', '2rem', '2rem'],
-                'tags': 'vr',
-                'id': 'JDQjc70a1T8',
                 'zoom': 16,
             }
         }
