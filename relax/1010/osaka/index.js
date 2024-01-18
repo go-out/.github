fec1af349470abdcec6077674429eeda5604a53e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-01-19 00:46:22 +0900

    Create index.js

---
+++ relax/1010/osaka/index.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const osaka268 = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.4683060723225,34.63517731681553]
+            },
+            'properties': {
+                'title': 'ゆぽぽ大運橋温泉',
+                'address': 'Yupopo 🇯🇵 Osaka, Japan',
+                'date': 'More Info',
+                'href': 'relax/?id=1010&area=osaka&name=yupopo',
+                'tags': 'yu',
+                'zoom': 17,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.51980129329303,34.59733330978709]
+            },
+            'properties': {
+                'title': '錦温泉',
+                'address': 'Nishiki Onsen 🇯🇵 Osaka, Japan',
+                'date': 'More Info',
+                'href': 'relax/?id=1010&area=osaka&name=nishiki',
+                'tags': 'yu',
+                'zoom': 17,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49530083077474,34.625889056290276]
+            },
+            'properties': {
+                'title': 'ニュー浴ハウスウェルネス',
+                'address': 'Wellness 🇯🇵 Osaka, Japan',
+                'date': 'More Info',
+                'href': 'relax/?id=1010&area=osaka&name=wellness',
+                'tags': 'yu',
+                'zoom': 17,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.51589014458102,34.621186349484475]
+            },
+            'properties': {
+                'title': 'いりふね温泉',
+                'address': 'Irihune Onsen 🇯🇵 Osaka, Japan',
+                'date': 'More Info',
+                'href': 'relax/?id=1010&area=osaka&name=irihune',
+                'tags': 'yu',
+                'zoom': 17,
+            }
+        }
+    ]
+}
