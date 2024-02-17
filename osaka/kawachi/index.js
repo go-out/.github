7c3303c4be05072ccb405fc28075918ac1d5d899
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-02-17 23:43:59 +0900

    osaka

--- osaka/kawachi/index.js
+++ osaka/kawachi/index.js
@@ -127,24 +127,6 @@ const osakaKawachi = {
             'type': 'Feature',
             'tags': 'draft',
             'feature': [
-                { 'month': 10 },
-                { 'month': 11 }
-            ],
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.55114031534507, 34.3734676593833]
-            },
-            'properties': {
-                'title': '岩湧山',
-                'address': '特に秋のススキの穂が広がる風景は大阪屈指の名スポット（9月下旬〜11月上旬）',
-                'date': '',
-                'zoom': 17.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'tags': 'draft',
-            'feature': [
                 { 'month': 11 }
             ],
             'geometry': {
