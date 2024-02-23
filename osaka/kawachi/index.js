c741313244e2b8c14f87c465266194e571c8b0f7
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-02-24 02:27:54 +0900

    osaka

--- osaka/kawachi/index.js
+++ osaka/kawachi/index.js
@@ -21,6 +21,20 @@ const osakaKawachi = {
         },
         {
             'type': 'Feature',
+            'tags': 'bicycle',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.58380755983578, 34.598111951563794]
+            },
+            'properties': {
+                'title': '地下鉄八尾南駅北自転車駐車場 レンタサイクル',
+                'address': '営業時間 7:00~19:00 料金 200円/1日（1回）',
+                'date': '',
+                'zoom': 16.5,
+            }
+        },
+        {
+            'type': 'Feature',
             'tags': 'legacy',
             'geometry': {
                 'type': 'Point',
@@ -38,26 +52,96 @@ const osakaKawachi = {
         {
             'type': 'Feature',
             'tags': 'draft',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.64415797733582, 34.71874238862553]
+            },
+            'properties': {
+                'title': '慈眼寺（野崎観音）',
+                'address': '境内から野崎城跡をへて吊り橋を通って飯盛山・飯盛山城跡などへ至る登山道がある。大阪みどりの百選に「野崎観音の緑」が選ばれている',
+                'date': '',
+                'zoom': 16,
+            }
+        },
+        {
+            'type': 'Feature',
+            'tags': 'draft',
             'feature': [
                 {
                     'month': 2,
-                    'text': '枚岡梅林（かおり風景100選）は、2月中旬から3月中旬が見頃'
+                    'text': '境内にある枚岡梅林（かおり風景100選）は、2月中旬から3月中旬が見頃'
                 },
                 {
                     'month': 3,
-                    'text': '枚岡梅林（かおり風景100選）は、2月中旬から3月中旬が見頃'
+                    'text': '境内にある枚岡梅林（かおり風景100選）は、2月中旬から3月中旬が見頃'
                 }
             ],
             'geometry': {
                 'type': 'Point',
-                'coordinates': [135.64995701713357, 34.66888318745667]
+                'coordinates': [135.65056489437342, 34.66990689214751]
             },
             'properties': {
                 'title': '枚岡神社',
-                'address': '神津嶽をご神体とする、神様の御霊が一杯の「神気」満ち満ちた神社',
+                'address': '神津嶽をご神体とする、神様の御霊が一杯の「神気」に満ちた神社',
                 'date': 'More Info',
                 'href': 'relax/?id=legacy&area=osaka&name=hiraoka-jinja',
                 'zoom': 16,
+            }
+        },
+        {
+            'type': 'Feature',
+            'tags': 'draft',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.6526278839939, 34.63485636718433]
+            },
+            'properties': {
+                'title': '玉祖神社',
+                'address': '',
+                'date': '',
+                'zoom': 16,
+            }
+        },
+        {
+            'type': 'Feature',
+            'tags': 'draft',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.63864143081364, 34.606506469571634]
+            },
+            'properties': {
+                'title': '恩智神社（天川山）',
+                'address': '',
+                'date': '',
+                'zoom': 14.5,
+            }
+        },
+        {
+            'type': 'Feature',
+            'tags': 'draft',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.63423024659778, 34.58978427192656]
+            },
+            'properties': {
+                'title': '鐸比古鐸比賣神社（高尾山）',
+                'address': '高尾山を神体山・磐座とする「ぬでひこ ぬでひめ じんじゃ」',
+                'date': '',
+                'zoom': 14.5,
+            }
+        },
+        {
+            'type': 'Feature',
+            'tags': 'draft',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.6184131203561, 34.576344418114886]
+            },
+            'properties': {
+                'title': '志貴縣主神社',
+                'address': '河内國総社',
+                'date': '',
+                'zoom': 16.5,
             }
         },
         {
@@ -86,90 +170,6 @@ const osakaKawachi = {
                 'address': '営業時間 7:00~19:00 料金 200円/1日（1回）',
                 'date': '',
                 'zoom': 16.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'tags': 'bicycle',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.58380755983578, 34.598111951563794]
-            },
-            'properties': {
-                'title': '地下鉄八尾南駅北自転車駐車場 レンタサイクル',
-                'address': '営業時間 7:00~19:00 料金 200円/1日（1回）',
-                'date': '',
-                'zoom': 16.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'tags': 'draft',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.64415797733582, 34.71874238862553]
-            },
-            'properties': {
-                'title': '慈眼寺（野崎観音）',
-                'address': '境内から野崎城跡をへて吊り橋を通って飯盛山・飯盛山城跡などへ至る登山道がある。大阪みどりの百選に「野崎観音の緑」が選ばれている',
-                'date': '',
-                'zoom': 16,
-            }
-        },
-        {
-            'type': 'Feature',
-            'tags': 'draft',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.6526689836174, 34.63463845963249]
-            },
-            'properties': {
-                'title': '玉祖神社',
-                'address': '',
-                'date': '',
-                'zoom': 16,
-            }
-        },
-        {
-            'type': 'Feature',
-            'tags': 'draft',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.6184131203561, 34.576344418114886]
-            },
-            'properties': {
-                'title': '志貴縣主神社',
-                'address': '河内國総社',
-                'date': '',
-                'zoom': 16.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'tags': 'draft',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.6465681782596, 34.60500476210762]
-            },
-            'properties': {
-                'title': '恩智神社（天川山）',
-                'address': '',
-                'date': '',
-                'zoom': 14.5,
-            }
-        },
-        {
-            'type': 'Feature',
-            'tags': 'draft',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [135.6465681782596, 34.60500476210762]
-            },
-            'properties': {
-                'title': '鐸比古鐸比賣神社（高尾山）',
-                'address': '高尾山を神体山・磐座とする「ぬでひこ ぬでひめ じんじゃ」',
-                'date': '',
-                'zoom': 14.5,
             }
         }
     ]
